2967983a9b0aa48d0cfb1d2763d519c2b3fe2bb5
Kris Pritchard <krisrp@users.noreply.github.com>
2020-03-23 17:33:33 +1300

    Fix bug

--- Week5/searching.py
+++ Week5/searching.py
@@ -4,8 +4,16 @@ random_list = [random.randint(1, 1000) for i in range(1000)]
 
 
 def my_search(some_list, number):
+    print('Starting search')
+    position = 0
     for i in range(len(random_list)):
+        position += 1
         if some_list[i] == number:
-            print('Found {} at index: {}'.format(number, i))
+            break
+
+    if position == len(some_list):
+        print('{} not found'.format(number))
+    else:
+        print('Found {} at index: {}'.format(number, i))
 
 my_search(random_list, 532)
