550479041fee1f16c0220fd343c650685ad492f1
Kris Pritchard <krisrp@users.noreply.github.com>
2020-03-23 15:31:01 +1300

    Add bubble_sort for Week 5

---
+++ Week5/bubble_sort.py
@@ -0,0 +1,20 @@
+my_list = [5, 2, 4, 3, 1]
+
+def bubble_sort(data):
+    print('bubble_sort')
+    passes, comparisons, swaps = 0, 0, 0
+    n = len(data)
+    for j in range(n - 1):
+        passes += 1
+        for i in range(n - j - 1):
+            comparisons += 1
+            current, next = data[i], data[i+1]
+            if current > next:
+                swaps += 1
+                print('{} > {}. Swap them'.format(current, next))
+                data[i], data[i+1] = next, current
+    print('Sorted: {}'.format(data))
+    print('Passes: {} - Comparisons: {} - Swaps: {}'.format(passes, comparisons, swaps))
+
+bubble_sort(my_list)
+
