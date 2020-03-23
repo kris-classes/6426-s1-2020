ae25530f2e5ec924b27596a07011ca9bb20bc4ab
Kris Pritchard <krisrp@users.noreply.github.com>
2020-03-23 17:12:04 +1300

    Add different example lists

--- Week5/bubble_sort.py
+++ Week5/bubble_sort.py
@@ -6,7 +6,11 @@ my_list = [5, 2, 4, 3, 1]
 sorted_list = [1, 2, 3, 4, 5]
 reversed_list = [5, 4, 3, 2, 1]
 
-random_list = [random.randint(1,100) for i in range(100)]
+ten_items = [random.randint(1,100) for i in range(10)]
+one_hundred_items = [random.randint(1,100) for i in range(100)]
+one_thousand_items = [random.randint(1,100) for i in range(1000)]
+ten_thousand_items = [random.randint(1,100) for i in range(10000)]
+one_hundred_thousand_items = [random.randint(1,100) for i in range(100000)]
 
 def bubble_sort(data):
     start = time.time()
@@ -20,12 +24,14 @@ def bubble_sort(data):
             current, next = data[i], data[i+1]
             if current > next:
                 swaps += 1
-                print('{} > {}. Swap them'.format(current, next))
+                #print('{} > {}. Swap them'.format(current, next))
                 data[i], data[i+1] = next, current
-    print('Sorted: {}'.format(data))
+    #print('Sorted: {}'.format(data))
     print('Passes: {} - Comparisons: {} - Swaps: {}'.format(passes, comparisons, swaps))
     stop = time.time()
     print('Time Taken: {} seconds'.format(stop - start))
 
 
-bubble_sort(random_list)
+bubble_sort(ten_items)
+bubble_sort(one_hundred_items)
+bubble_sort(one_thousand_items)
