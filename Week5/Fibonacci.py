6492dbce13d58d93ab9f15afb4c0e5d6dfb78e6c
Muzz00 <57882850+Muzz00@users.noreply.github.com>
2020-03-26 13:15:53 +1300

    Fibonacci.py

--- Week5/Fibonacci.py
+++ Week5/Fibonacci.py
@@ -1,6 +1,32 @@
+from functools import lru_cache
+
+
+@lru_cache(maxsize=100)
 def fibonacci(n):
+    if type(n) != int:
+        raise TypeError("n must be a positive number")
+    if n < 1:
+        raise ValueError("n must be a positive number")
     if n == 1:
         return 1
-    elif n == 2:
+    if n == 2:
         return 2
-    else:
+    if n > 2:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+for i in range(1, 101):
+    print(i, ";", fibonacci(i))
+
+# def fibonacci(n):
+#    if n in fibonacciCache:
+#        return fibonacciCache[n]
+#    if n == 1:
+#        return 1
+#    elif n == 2:
+#        return 2
+#    elif n > 2:
+#        value = fibonacci(n-1) + fibonacci(n-2)
+#        fibonacciCache[n] = value
+#        return value
+# fibonacciCache = {}
