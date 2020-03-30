913b2310a21d515a327ecffe0e64f36863a6e723
Kris Pritchard <krisrp@users.noreply.github.com>
2020-03-30 17:05:37 +1300

    Add week 6 files

---
+++ Week6/hello.py
@@ -0,0 +1,26 @@
+def say_hello():
+    print('about to say hello')
+    #import pdb; pdb.set_trace()
+    #breakpoint()
+    print('hello')
+    print('123')
+    print('456')
+    print('3482')
+    blahblah()
+    print('aaaa')
+    print('bbbb')
+    print('cccc')
+    print('dddd')
+
+
+def blahblah():
+    print("blah")
+    print("bleep")
+    print("bloop")
+    return 123
+
+print(__name__)
+
+if __name__ == '__main__':
+    print('sJHFDHJHSJFKHFJKDSFHJKDHJKFDSHJKFDS')
+    say_hello()
