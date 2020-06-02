17a56d2e66929032dafa29443a3eec8a790c321c
reuben <reusharino@gmail.com>
2020-06-02 16:20:23 +1200

    made draw multiple trees

--- Assignment/reusharino/L-System Assignment/Attempt 2.py
+++ Assignment/reusharino/L-System Assignment/Attempt 2.py
@@ -2,7 +2,7 @@ import turtle as tu
 
 foo = tu.Turtle()
 foo.left(90)
-foo.speed(10000)
+foo.speed(1000000)
 
 
 def draw(l, n):
@@ -11,24 +11,36 @@ def draw(l, n):
     else:
         foo.forward(l)
         foo.left(30)
-        draw(l / 2, n)
+        draw(l / 1.5, n)
         foo.right(60)
-        draw(l / 2, n)
+        draw(l / 1.5, n)
         foo.left(30)
+        # foo.dot()
         foo.backward(l)
+        # foo.clear()
+
 
 
 d = int(input('Input number: '))
 x = input('type y to draw: ')
 
-draw(d, 100)
-draw(d, 50)
-draw(d, 25)
-draw(d, 10)
-draw(d, 5)
-draw(d, 1)
-draw(d, 0.1)
-draw(d, 0.01)
 
+foo.setposition(-1000, 0)
+draw(d, 100)
+
+foo.setposition(-650, 0)
+draw(d, 50)
+
+foo.setposition(-300, 0)
+draw(d, 25)
+
+foo.setposition(50, 0)
+draw(d, 10)
+
+foo.setposition(400, 0)
+draw(d, 5)
+
+foo.setposition(750, 0)
+draw(d, 1)
 
 input("press enter to stop")
