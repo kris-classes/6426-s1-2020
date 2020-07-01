51164415588e0666b0e23cc07c0f59f8a88ad8fb
Muzz00 <57882850+Muzz00@users.noreply.github.com>
2020-07-01 18:10:34 +1200

    Random Created

--- Assignment/Muzz00/BreadthFirstSearch.py
+++ Assignment/Muzz00/BreadthFirstSearch.py
@@ -1,5 +1,6 @@
 import pyxel
 import time
+import random
 #PLEASE REFER TO FIRST TO APPENDIX/INFORMATION OF THE BREADTH FIRST SEARCH DATA STRUCTURE
 
 class Node:
@@ -18,22 +19,31 @@ class Node:
     def __repr__(self):
         return self.data
 
-#Nodes are created below
-n1 = Node('1')
-n2 = Node('2')
-n3 = Node('3')
-n4 = Node('4')
-n5 = Node('5')
-n6 = Node('6')
-n7 = Node('7')
-n8 = Node('8')
-n9 = Node('9')
-n10 = Node('10')
-n11 = Node('11')
-n12 = Node('12')
-n13 = Node('13')
-n14 = Node('14')
-n15 = Node('15')
+
+# Randomly Generate Num
+rand = []
+for i in range(15):
+    randNum = ""
+    while randNum == "" or randNum in rand:
+        randNum = random.randint(0, 100)
+    rand.append(randNum)
+
+# Nodes are created below
+n1 = Node(str(rand[0]))
+n2 = Node(str(rand[1]))
+n3 = Node(str(rand[2]))
+n4 = Node(str(rand[3]))
+n5 = Node(str(rand[4]))
+n6 = Node(str(rand[5]))
+n7 = Node(str(rand[6]))
+n8 = Node(str(rand[7]))
+n9 = Node(str(rand[8]))
+n10 = Node(str(rand[9]))
+n11 = Node(str(rand[10]))
+n12 = Node(str(rand[11]))
+n13 = Node(str(rand[12]))
+n14 = Node(str(rand[13]))
+n15 = Node(str(rand[14]))
 
 n1.nodes = [n2, n3]
 n2.nodes = [n4, n5, n1]
@@ -55,7 +65,7 @@ n15.nodes = [n8]
 pyxel.init(250, 150)
 
 
-nodeSelector = n1 #Default Node
+nodeSelector = n1 # Default Node
 
 
 def update():
@@ -76,8 +86,8 @@ def draw():
     pyxel.cls(0)
     pyxel.text(0, 5, "Press 1, 2 or 3", 15)
     pyxel.text(0, 15, "to switch Node", 15)
-    node = nodeSelector #Option of User if not default
-    #Default settings
+    node = nodeSelector # Option of User if not default
+    # Default settings
     node.xCount = len(node.nodes)
     node.x = 125
     node.y = 10
@@ -87,41 +97,41 @@ def draw():
     while queue:
         curNode = queue.pop(0)
 
-        xCount = 0 #Internal counter of Nodes of a given Node
+        xCount = 0 # Internal counter of Nodes of a given Node
 
-        #Calulate length of a Node
+        # Calulate length of a Node
         lenNode = len(curNode.nodes)
         if curNode.data != node.data:
-            lenNode -= 1 #Length of Node - 1 as the parent node wont be in the same horizontal x axis as the other child connected nodes.
+            lenNode -= 1 # Length of Node - 1 as the parent node wont be in the same horizontal x axis as the other child connected nodes.
 
-        #Refer to appendix 1 for xLeft and appendix 2 for newXSpacing
+        # Refer to appendix 1 for xLeft and appendix 2 for newXSpacing
 
-        #Calculating the childs spacing of a given curNode
+        # Calculating the childs spacing of a given curNode
         if lenNode != 0:
-            temp = (curNode.x - curNode.xLeft) * 2  #Calculates parents spacing dynamically. get the curNode.x e.g. 1's is 125. Minus that with horizontal spacing, which gives half spacing of parent element as Nodes are located on the center of spacing, hence multiple by 2.
-            newXSpacing = (temp / lenNode)  #Calulate child spacing. ParentSpacing/numOfChild=ChildSpacing
+            temp = (curNode.x - curNode.xLeft) * 2  # Calculates parents spacing dynamically. get the curNode.x e.g. 1's is 125. Minus that with horizontal spacing, which gives half spacing of parent element as Nodes are located on the center of spacing, hence multiple by 2.
+            newXSpacing = (temp / lenNode)  # Calulate child spacing. ParentSpacing/numOfChild=ChildSpacing
 
 
         for n in curNode.nodes:
             if n not in visited:
                 n.parent = curNode
-                n.x = (xCount * newXSpacing) + (newXSpacing / 2) #Calculating x axis location. (InternalCounter * SpacingsOfChild) calculates the margin left of the node block. (newXSpacing/2) centers the node within its block rather than floating left.
+                n.x = (xCount * newXSpacing) + (newXSpacing / 2) # Calculating x axis location. (InternalCounter * SpacingsOfChild) calculates the margin left of the node block. (newXSpacing/2) centers the node within its block rather than floating left.
 
-                #Add xLeft of Parent to x and also to xLeft. We have to loop through the visited as we need to get the stored data that was produced dynamically.
+                # Add xLeft of Parent to x and also to xLeft. We have to loop through the visited as we need to get the stored data that was produced dynamically.
                 n.xLeft = xCount * newXSpacing
                 for element in visited:
                     if element.data == curNode.data:
                         n.x += element.xLeft
                         n.xLeft += element.xLeft
 
-                n.y = curNode.y + 25 #Add vertical spacing to each node
+                n.y = curNode.y + 25 # Add vertical spacing to each node
 
                 visited.append(n)
                 queue.append(n)
                 xCount += 1
 
-        #DRAWINGS
-        #DRAW LINES FIRST
+        # DRAWINGS
+        # DRAW LINES FIRST
         for element in visited:
             if element.parent is not None:
                 x1 = element.parent.x
@@ -129,7 +139,7 @@ def draw():
                 x2 = element.x
                 y2 = element.y
                 pyxel.line(x1, y1, x2, y2, 14)
-        #DRAW CIRCLES
+        # DRAW CIRCLES
         for element in visited:
             pyxel.circ(element.x, element.y, 6, 12)
             pyxel.text(element.x - (len(element.data) * 1.5), element.y - 1, element.data, 2)
---
+++ Assignment/Muzz00/README.md
@@ -0,0 +1,14 @@
+#Task 3 Analysis and Commentary
+
+###Q1) A paragraph documenting issues you encountered in the design or implementation of your chosen data structure/algorithm.
+#### There are many issues that I encountered while designing and implementing my chosen algorithm. The biggest issue that I encountered was to dynamically space all the nodes 
+
+###Q2) Briefly explain the strengths and weaknesses of your data structure or algorithm with respect to resource consumption. Under what conditions does it perform the best or worst?
+####here
+
+###Q3) List a real-world application of your chosen data structure or algorithm.
+####here
+
+###Q4) A sentence or two on the asymptotic worst-case time and space complexity of your chosen data structure/algorithm.
+####Here
+
