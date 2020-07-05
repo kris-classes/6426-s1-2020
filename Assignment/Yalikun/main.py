6bd6f2d1c0e5c3b19c6391e612343692c87f207f
Yalikun <yalikuny@gmail.com>
2020-07-05 18:58:10 +1200

    Update main.py

--- Assignment/Yalikun/main.py
+++ Assignment/Yalikun/main.py
@@ -44,12 +44,6 @@ class Node:
 
 
 
-def minheap():
-    return True
-def maxheap():
-    return False
-
-
 
 
 
@@ -68,27 +62,6 @@ print(randomlist)
 
 
 
-outputlist = []
-
-
-
-if minheap() is True:
-    minheapnumlist = randomlist
-    heapq.heapify(minheapnumlist)
-    print('min')
-    print(minheapnumlist)
-    for j in minheapnumlist:
-        j = str(j)
-        outputlist.append(j)
-
-elif maxheap() is True:
-    maxheapnumlist = randomlist
-    heapq._heapify_max(maxheapnumlist)
-    print('max')
-    print(maxheapnumlist)
-    for k in maxheapnumlist:
-        k = str(k)
-        outputlist.append(k)
 
 
 
@@ -98,8 +71,7 @@ elif maxheap() is True:
 
 
 
-print('output')
-print(outputlist)
+
 
 
 
@@ -108,47 +80,103 @@ class App:
         pyxel.init(256, 180, caption="Min heap visualization")
         pyxel.mouse(True)
 
-        #self.root = None
-        #self.generate_tree()
+        self.outputlist = []
+        self.heap()
+
+
+
+
+
 
         self.n1 = Node(128, 30, 6, 8)
 
         self.n2 = Node(70, 65, 6, 8)
         self.n3 = Node(186, 65, 6, 8)
 
-        if len(outputlist) >= 4:
+        if self.size >= 4:
             self.n4 = Node(35, 100, 6, 8)
-        if len(outputlist) >= 5:
+        if self.size >= 5:
             self.n5 = Node(100, 100, 6, 8)
-        if len(outputlist) >= 6:
+        if self.size >= 6:
             self.n6 = Node(155, 100, 6, 8)
-        if len(outputlist) >= 7:
+        if self.size >= 7:
             self.n7 = Node(220, 100, 6, 8)
 
-        if len(outputlist) >= 8:
+        if self.size >= 8:
             self.n8 = Node(20, 135, 6, 8)
-        if len(outputlist) >= 9:
+        if self.size >= 9:
             self.n9 = Node(50, 135, 6, 8)
-        if len(outputlist) >= 10:
+        if self.size >= 10:
             self.n10 = Node(85, 135, 6, 8)
-        if len(outputlist) >= 11:
+        if self.size >= 11:
             self.n11 = Node(115, 135, 6, 8)
-        if len(outputlist) >= 12:
+        if self.size >= 12:
             self.n12 = Node(140, 135, 6, 8)
-        if len(outputlist) >= 13:
+        if self.size >= 13:
             self.n13 = Node(170, 135, 6, 8)
-        if len(outputlist) >= 14:
+        if self.size >= 14:
             self.n14 = Node(205, 135)
-        if len(outputlist) >= 15:
+        if self.size >= 15:
             self.n15 = Node(235, 135, 6, 8)
 
         pyxel.cls(0)
 
+
         pyxel.run(self.update, self.draw)
 
 
+
+
+
+
+    def minheap(self):
+        return True
+
+    def maxheap(self):
+        return False
+
+
+    @property
+    def size(self):
+        return len(self.outputlist)
+
+    def heap(self):
+
+        if self.minheap() is True:
+            minheapnumlist = randomlist
+            heapq.heapify(minheapnumlist)
+            print('min')
+            print(minheapnumlist)
+            for j in minheapnumlist:
+                j = str(j)
+                self.outputlist.append(j)
+
+        elif self.maxheap() is True:
+            maxheapnumlist = randomlist
+            heapq._heapify_max(maxheapnumlist)
+            print('max')
+            print(maxheapnumlist)
+            for k in maxheapnumlist:
+                k = str(k)
+                self.outputlist.append(k)
+
+
+
+
+
     def update(self):
-        pass
+        if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON):
+            x = pyxel.mouse_x
+            y = pyxel.mouse_y
+
+            if x < 85 and y < 15:
+                pyxel.play(0, 1)
+            elif 85 < x < 170 and y < 15:
+                pyxel.play(0, 1)
+            elif 170 < x < 255 and y < 15:
+                pyxel.play(0, 1)
+
+
 
     def draw(self):
         pyxel.cls(0)
@@ -167,64 +195,64 @@ class App:
 
         # n1 to n3
         self.n1.draw()
-        pyxel.text(125, 28, outputlist[0], 7)
+        pyxel.text(125, 28, self.outputlist[0], 7)
         self.n2.draw()
-        pyxel.text(67, 63, outputlist[1], 7)
+        pyxel.text(67, 63, self.outputlist[1], 7)
         pyxel.line(128, 37, 70, 58, 11)
         self.n3.draw()
-        pyxel.text(183, 63, outputlist[2], 7)
+        pyxel.text(183, 63, self.outputlist[2], 7)
         pyxel.line(128, 37, 186, 58, 11)
 
 # n4 to n7
-        if len(outputlist) >= 4:
+        if self.size >= 4:
             self.n4.draw()
-            pyxel.text(32, 98, outputlist[3], 7)
+            pyxel.text(32, 98, self.outputlist[3], 7)
             pyxel.line(70, 72, 32, 98, 11)
-        if len(outputlist) >= 5:
+        if self.size >= 5:
             self.n5.draw()
-            pyxel.text(97, 98, outputlist[4], 7)
+            pyxel.text(97, 98, self.outputlist[4], 7)
             pyxel.line(70, 72, 100, 93, 11)
-        if len(outputlist) >= 6:
+        if self.size >= 6:
             self.n6.draw()
-            pyxel.text(152, 98, outputlist[5], 7)
+            pyxel.text(152, 98, self.outputlist[5], 7)
             pyxel.line(186, 72, 155, 93, 11)
-        if len(outputlist) >= 7:
+        if self.size >= 7:
             self.n7.draw()
-            pyxel.text(217, 98, outputlist[6], 7)
+            pyxel.text(217, 98, self.outputlist[6], 7)
             pyxel.line(186, 72, 220, 93, 11)
 
 # n8 to n15
-        if len(outputlist) >= 8:
+        if self.size >= 8:
             self.n8.draw()
-            pyxel.text(17, 133, outputlist[7], 7)
+            pyxel.text(17, 133, self.outputlist[7], 7)
             pyxel.line(35, 107, 20, 128, 11)
-        if len(outputlist) >= 9:
+        if self.size >= 9:
             self.n9.draw()
-            pyxel.text(47, 133, outputlist[8], 7)
+            pyxel.text(47, 133, self.outputlist[8], 7)
             pyxel.line(35, 107, 50, 128, 11)
-        if len(outputlist) >= 10:
+        if self.size >= 10:
             self.n10.draw()
-            pyxel.text(82, 133, outputlist[9], 7)
+            pyxel.text(82, 133, self.outputlist[9], 7)
             pyxel.line(100, 107, 85, 128, 11)
-        if len(outputlist) >= 11:
+        if self.size >= 11:
             self.n11.draw()
-            pyxel.text(112, 133, outputlist[10], 7)
+            pyxel.text(112, 133, self.outputlist[10], 7)
             pyxel.line(100, 107, 115, 128, 11)
-        if len(outputlist) >= 12:
+        if self.size >= 12:
             self.n12.draw()
-            pyxel.text(137, 133, outputlist[11], 7)
+            pyxel.text(137, 133, self.outputlist[11], 7)
             pyxel.line(155, 107, 140, 128, 11)
-        if len(outputlist) >= 13:
+        if self.size >= 13:
             self.n13.draw()
-            pyxel.text(167, 133, outputlist[12], 7)
+            pyxel.text(167, 133, self.outputlist[12], 7)
             pyxel.line(155, 107, 170, 128, 11)
-        if len(outputlist) >= 14:
+        if self.size >= 14:
             self.n14.draw()
-            pyxel.text(202, 133, outputlist[13], 7)
+            pyxel.text(202, 133, self.outputlist[13], 7)
             pyxel.line(220, 107, 205, 128, 11)
-        if len(outputlist) >= 15:
+        if self.size >= 15:
             self.n15.draw()
-            pyxel.text(232, 133, outputlist[14], 7)
+            pyxel.text(232, 133, self.outputlist[14], 7)
             pyxel.line(220, 107, 235, 128, 11)
 
 
@@ -232,6 +260,7 @@ class App:
 
 if __name__ == '__main__':
     App()
+
 
 
 
