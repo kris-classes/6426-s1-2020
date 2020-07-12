a023e0e279094059c55ad5b87b5c12137afb2068
chaosay <38771541+chaosay@users.noreply.github.com>
2020-07-12 15:29:41 +1200

    Update main.py
    uploaded set version

--- Assignment/chaosay/main.py
+++ Assignment/chaosay/main.py
@@ -16,13 +16,13 @@ Presets: (Num keys)
 3: Exploder
 4: Lightweight space ship
 5: Blinker
-Warning: It's possible to spawn multiple presets ontop of each other, which means they will vanish right away.
 ###############################
 Game rules/logic:
 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 2. Any live cell with two or three live neighbours lives on to the next generation.
 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
 More simply:
 1. any live cell with two or three neighbors lives
 2. any dead cell with three live neighbors becomes a live cell
@@ -31,7 +31,60 @@ More simply:
 import pyxel
 import time
 
-alive_cells = []
+# additional data structure
+class Set:
+    """
+    Basic implementation of a set.
+    Set must NOT contain duplicates.
+    """
+
+    def __init__(self, initial_data=[]):
+        # NOTE: I've done this for you to show how it's done.
+        # self.data MUST remain a list throughout.
+        unique_only = []
+        self.current = 0
+        for element in initial_data:
+            if element not in unique_only:
+                unique_only.append(element)
+        self.data = unique_only
+
+    def __str__(self):
+        return f'<Set __str__: {self.data}>'
+
+    def __repr__(self):
+        # Used when you type my_set into the shell.
+        return f'<Set __repr__: {self.data}>'
+
+    def __add__(self, other):
+        result = []
+        for new_item in other:
+            if new_item not in self.data:
+                result.append(new_item)
+        return self.data+result
+
+    def __contains__(self, element): # used default contains for the list
+        if hasattr(self.data[0], "__contains__"):
+            return self.data[0].__contains__(element)
+        return self.data.__contains__(element)
+
+    def __iter__(self): # uses default iterator for the list
+        if hasattr(self.data[0], "__iter__"):
+            return self.data[0].__iter__()
+        return self.data.__iter__()
+
+    def append(self, new_item):
+        if new_item not in self.data:
+            self.data.append(new_item)
+
+    def remove(self, element):
+        # Remove an element from a set. Raise an exception if it does not exist
+        if element in self.data:
+            self.data.remove(element)
+        else:
+            raise Exception("Element not found in set")
+
+# define some variables
+alive_cells = Set()
 dead_cell_scans = [] # keeps track of all tested dead cells, reset every simulation tick
 test_cells = [] # test ONLY, used here so the test cells redraw
 reviving_cells = []
@@ -40,7 +93,6 @@ lastupdate = 0.0
 cellID = 0
 gameActive = False
 debug = False
-sim_frame_active = False
 sim_speed = 8
 
 # code used for anything
@@ -95,7 +147,7 @@ class App:
     def __init__(self):
         global alive_cells
         # create a glider on start
-        alive_cells = [simCell(25,25), simCell(26,26), simCell(24,27), simCell(25,27), simCell(26,27)]  # x, y, alive
+        alive_cells = Set([simCell(25,25), simCell(26,26), simCell(24,27), simCell(25,27), simCell(26,27)])  # x, y, alive
         # Initialize a window. Max size is 256x256 pixels.
         pyxel.init(50, 50)
         self.x = 50
@@ -176,8 +228,8 @@ class App:
             alive_cells += spaceShip
 
         if pyxel.btnp(pyxel.KEY_5):  # blinker
-            startPosX = 40
-            startPosY = 45
+            startPosX = 50
+            startPosY = 20
             blinker = [simCell(startPosX, startPosY),  # row 1
                       simCell(startPosX, startPosY + 1),  # row 2
                       simCell(startPosX, startPosY + 2)]  # row 3
@@ -218,7 +270,6 @@ class App:
             global new_pixels
             global reviving_cells
             global dying_cells
-            global sim_frame_active
 
             # The following code handles all of the simulation logic. Referred to as a tick.
             if pyxel.frame_count%sim_speed == 1: # causes simulation to only update every x number of frames, otherwise it's too fast
